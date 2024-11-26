b522569f9a3593ee21bfa20a7fde0f61caf898f0
Paulo Cesar <paulocesarmedeiross@gmail.com>
2024-11-26 15:29:24 -0300

    ajustes em pagina nao usada

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,11 +5,8 @@ import { HeaderProps } from '@/components/Header';
 import LandingProducts from '@/components/LandingProducts';
 import MainLayout from '@/layouts/MainLayout';
 import { Product } from '@/types/Product';
-import { listCartItems, listProducts } from '@/services/products';
-import { ItemCheckout } from '@/components/ItemCheckout';
+import { listProducts } from '@/services/products';
 import { useEffect, useState } from 'react';
-import { CartItem } from '@/types/CartItem';
-import Checkout from '@/components/Checkout';
 
 const header: HeaderProps = {
   title: 'Livre Mercado',
@@ -31,20 +28,14 @@ const footer: FooterProps = {
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
-  // const [cart, setCart] = useState<CartItem[]>([]);
 
   useEffect(() => {
     listProducts({ page: 0, size: 10 }).then(setProducts);
   }, []);
 
-  // useEffect(() => {
-  //   listCartItems().then(setCart);
-  // }, []);
-
   return (
     <MainLayout header={header} footer={footer}>
       <LandingProducts products={products}></LandingProducts>
-      {/* <Checkout products={cart}></Checkout> */}
     </MainLayout>
   );
 }

6a10da203e139dc307f977d1e2f4bfb7092c51c1
Roberto Dantas <7robertodantas@gmail.com>
2024-11-26 08:19:54 -0300

    Send pagination params in products fetch

--- frontend/src/services/products.ts
+++ frontend/src/services/products.ts
@@ -4,7 +4,12 @@ import { Product } from "@/types/Product";
 
 
 export async function listProducts({ page, size }: { page: number, size: number }): Promise<Product[]> {
-  const response = await fetch(`http://localhost:4000/products`, {
+  const params = new URLSearchParams({
+    'page': page ? page.toString() : '0',
+    'size': size ? size.toString() : '10'
+  });
+
+  const response = await fetch(`http://localhost:4000/products?${params}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',

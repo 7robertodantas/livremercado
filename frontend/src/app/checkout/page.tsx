69e8a07e79c1745525a1f3e150fb854d495c3698
Paulo Cesar <paulocesarmedeiross@gmail.com>
2024-11-26 15:39:11 -0300

    ajustes

--- frontend/src/app/checkout/page.tsx
+++ frontend/src/app/checkout/page.tsx
@@ -57,20 +57,8 @@ const footer: FooterProps = {
 
 
 export default function Home() {
-  //const [products, setProducts] = useState<Product[]>([]);
-  // const [cart, setCart] = useState<CartItem[]>([]);
-
-//   useEffect(() => {
-//     listProducts({ page: 0, size: 10 }).then(setProducts);
-//   }, []);
-
-  // useEffect(() => {
-  //   listCartItems().then(setCart);
-  // }, []);
-
   return (
     <MainLayout header={header} footer={footer}>
-      {/* <LandingProducts products={products}></LandingProducts> */}
       <Checkout products={cart}></Checkout>
     </MainLayout>
   );

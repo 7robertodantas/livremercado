e7b4ea4e7e5b655aac885bfc67ef7c60e8c60704
Herlmanoel Fernandes <35230448+herlmanoel@users.noreply.github.com>
2024-11-28 10:50:27 -0300

    Add product details and back-end integration (#15)
    * feat: add product details and back-end integration
    
    * fix: add max-width img

--- frontend/src/app/products/[id]/page.tsx
+++ frontend/src/app/products/[id]/page.tsx
@@ -1,25 +1,266 @@
-'use client';
+"use client";
 
-import MainLayout from '@/layouts/MainLayout';
-import { Product } from '@/types/Product';
-import { getProductById } from '@/services/products';
-import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
-import ProductCard from '@/components/ProductCard';
+import React, { useState, useEffect } from "react";
+import {
+  HiOutlineHeart,
+  HiHeart,
+  HiOutlineCheck,
+  HiOutlineShieldCheck,
+  HiOutlineLocationMarker,
+} from "react-icons/hi";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Product } from "@/types/Product";
+import { Seller } from "@/types/Seller";
+import { getProductById, updateProductFavorite } from "@/services/products";
+import { getSellerById } from "@/services/sellers";
+import { useParams } from "next/navigation";
 
-export default function PageComponent() {
+import {
+  Column,
+  ColumnProduct,
+  ColumnProductChild,
+  Container,
+  ContainerProductDetails,
+  ContainerWrapper,
+  Gallery,
+  InfoContainer,
+  InfoDescription,
+  InfoTitle,
+  Panel,
+  ProductActionActions,
+  ProductActionBenefits,
+  ProductActionButton,
+  ProductActionCategory,
+  ProductActionContainer,
+  ProductActionMethodCard,
+  ProductActionPriceCard,
+  ProductActionPriceRow,
+  ProductActionRowTitle,
+  ProductContainerHeader,
+  Row,
+  SellerInfoLocationCard,
+  SellerInfoMore,
+  SellerInfoReputationCard,
+  SellerInfoReputationRow,
+  SellerInfoReputationThermometer,
+  SellerInfoTitle,
+  StyledSellerInfo,
+  Wrapper,
+} from "./styles";
+
+export default function ProductPage() {
   const params = useParams();
-  const [product, setProduct] = useState<Product>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [seller, setSeller] = useState<Seller | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [isFavorited, setIsFavorited] = useState(false);
 
   useEffect(() => {
-    if (params.id) {
-      getProductById({ id: params.id.toString() }).then(setProduct);
+    async function fetchData() {
+      try {
+        if (params?.id) {
+          const productData = await getProductById({ id: Array.isArray(params.id) ? params.id[0] : params.id });
+          setProduct(productData);
+          setIsFavorited(productData.isFavorite);
+          console.log(productData);
+
+          if (productData.sellerId) {
+            const sellerData = await getSellerById({ id: String(productData.sellerId) });
+            setSeller(sellerData);
+            console.log("sellerData: " + sellerData);
+          } else {
+            console.warn("Produto não possui um sellerId:", productData);
+          }
+        }
+      } catch (error) {
+        console.error("Erro ao carregar dados:", error);
+      } finally {
+        setLoading(false);
+      }
     }
+
+    fetchData();
   }, [params]);
 
+  const toggleFavorite = async () => {
+    if (!product) return;
+  
+    const updatedFavorite = !isFavorited;
+  
+    try {
+      setIsFavorited(updatedFavorite);
+  
+      await updateProductFavorite(product.id, updatedFavorite);
+  
+      console.log(
+        `Produto "${product.title}" ${
+          updatedFavorite ? "adicionado aos" : "removido dos"
+        } favoritos.`
+      );
+    } catch (error) {
+      setIsFavorited(!updatedFavorite);
+  
+      console.error(
+        `Erro ao atualizar favorito para o produto "${product.title}":`,
+        error
+      );
+    }
+  };
+
+  if (loading) {
+    return <div>Carregando detalhes do produto...</div>;
+  }
+
+  if (!product) {
+    return <div>Produto não encontrado.</div>;
+  }
+
   return (
-    <MainLayout>
-      {product && <ProductCard product={product} />}
-    </MainLayout>
+    <>
+    <ContainerProductDetails>
+      <Header title="Detalhes do Produto" links={[]} />
+      <ContainerWrapper>
+        <Wrapper>
+          <div>
+            <Container>
+              <Row>
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  Compartilhar
+                </a>
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  Vender um igual
+                </a>
+              </Row>
+
+              <Panel>
+                <Column>
+                  <ColumnProduct>
+                    <ColumnProductChild>
+                      <Gallery>
+                        <img alt={product.title} src={product.imageUrl} />
+                      </Gallery>
+                      <ProductContainerHeader>
+                        <ProductActionCategory>{product.category}</ProductActionCategory>
+                        <ProductActionRowTitle>
+                          <h1>{product.title}</h1>
+                          {isFavorited ? (
+                            <HiHeart
+                              color="#ff5252"
+                              size={28}
+                              onClick={toggleFavorite}
+                              aria-label="Remover dos favoritos"
+                              role="button"
+                            />
+                          ) : (
+                            <HiOutlineHeart
+                              color="var(--color-blue)"
+                              size={28}
+                              onClick={toggleFavorite}
+                              aria-label="Adicionar aos favoritos"
+                              role="button"
+                            />
+                          )}
+                        </ProductActionRowTitle>
+
+                        <ProductActionPriceCard>
+                          <ProductActionPriceRow>
+                            <span className="symbol">R$</span>
+                            <span className="fraction">{product.price.toFixed(2)}</span>
+                          </ProductActionPriceRow>
+                        </ProductActionPriceCard>
+                      </ProductContainerHeader>
+                    </ColumnProductChild>
+                  </ColumnProduct>
+                  <Info description={product.description} />
+                </Column>
+
+                <Column>
+                  <ProductActionContainer>
+                    <ProductActionMethodCard>
+                      <HiOutlineCheck size={24} color="var(--color-green)" />
+                      <div>
+                        <span className="title">Frete grátis</span>
+                        <span className="details">Benefício Lorem Ipsum</span>
+                      </div>
+                    </ProductActionMethodCard>
+
+                    <ProductActionActions>
+                      <ProductActionButton solid>
+                        Comprar agora
+                      </ProductActionButton>
+                      <ProductActionButton>Adicionar ao carrinho</ProductActionButton>
+                    </ProductActionActions>
+
+                    <ProductActionBenefits>
+                      <li>
+                        <HiOutlineShieldCheck color="var(--color-gray)" size={30} />
+                        <p>
+                          Compra Garantida, receba o produto que está esperando ou devolvemos
+                          seu dinheiro.
+                        </p>
+                      </li>
+                    </ProductActionBenefits>
+                  </ProductActionContainer>
+                  {seller && <SellerInfo seller={seller} />}
+                </Column>
+              </Panel>
+            </Container>
+          </div>
+        </Wrapper>
+      </ContainerWrapper>
+      <Footer text="texto footer" />
+    </ContainerProductDetails>
+    
+    </>
   );
 }
+
+interface InfoProps {
+  description: string;
+}
+
+const Info: React.FC<InfoProps> = ({ description }) => (
+  <InfoContainer>
+    <InfoTitle>Descrição</InfoTitle>
+    <InfoDescription>{description}</InfoDescription>
+  </InfoContainer>
+);
+
+interface SellerInfoProps {
+  seller: Seller;
+}
+
+const SellerInfo: React.FC<SellerInfoProps> = ({ seller }) => (
+  <StyledSellerInfo>
+    <SellerInfoTitle>Informações sobre o vendedor</SellerInfoTitle>
+    <SellerInfoLocationCard>
+      <HiOutlineLocationMarker />
+      <div>
+        <p>Localização</p>
+        <strong>{seller.location}</strong>
+      </div>
+    </SellerInfoLocationCard>
+    <SellerInfoReputationCard>
+      <SellerInfoReputationThermometer>
+        {[...Array(5)].map((_, index) => (
+          <li key={index} className={index < seller.reputation ? "active" : ""}></li>
+        ))}
+      </SellerInfoReputationThermometer>
+      <SellerInfoReputationRow>
+        <div>
+          <strong>{seller.salesLastMonths}</strong>
+          <span>vendas nos últimos 4 meses</span>
+        </div>
+        <div>
+          <strong>{seller.goodSupport ? "👍" : "👎"}</strong>
+          <span>Presta um bom atendimento</span>
+        </div>
+        <div>
+          <strong>{seller.onTimeDelivery ? "⏰" : "❌"}</strong>
+          <span>Entrega os produtos dentro do prazo</span>
+        </div>
+      </SellerInfoReputationRow>
+    </SellerInfoReputationCard>
+  </StyledSellerInfo>
+);
---
+++ frontend/src/app/products/[id]/styles.tsx
@@ -0,0 +1,398 @@
+import styled from 'styled-components';
+
+export const ContainerProductDetails = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: #F5F5F5;
+
+    flex-direction: column;
+
+
+justify-content: space-between;
+
+display: flex;
+    
+
+    color: #333333;
+`;
+
+export const ContainerWrapper = styled.div`
+    max-width: 1220px;
+    margin: 0 auto;
+    padding: 0 16px;
+    border-radius: 80px;
+    display: flex;
+    flex-direction: column;
+
+
+    justify-content: space-between;
+
+    display: flex;
+    flex: 1;
+`;
+
+export const Wrapper = styled.main`
+    max-width: 1220px;
+    margin: 0 auto;
+`;
+
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column; 
+`;
+
+export const Row = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 16px 0;
+
+    > a {
+        font-size: 14px;
+        text-decoration: none;
+        color: #3483fa;
+        padding: 2.5px 0;
+    }
+
+    > a + a {
+        padding-left: 10px;
+        margin-left: 10px;
+        border-left: 1px solid #ddd;
+    }
+`;
+
+export const Panel = styled.div`
+    background: #FFF;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 20px;
+    padding: 40px;
+    border-radius: 10px;
+
+    > div:first-child {
+        flex: 0 0 70%;
+    }
+
+    > div:last-child {
+        flex: 1;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        font-size: 14px;
+        padding: 25px 16px;
+        width: 100%;
+    }
+`;
+
+export const Column = styled.div``;
+
+export const ColumnProduct = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ColumnProductChild = styled.div`
+    display: flex;
+
+    > div {
+        flex: 1;
+    }
+`;
+
+export const Gallery = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 530px;
+
+    > img {
+        max-width: 450px;
+    }
+`;
+
+export const ProductContainerHeader = styled.div`
+    padding: 32px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+`;
+
+export const ProductActionCategory = styled.div`
+    font-size: 16px;
+    color: #333;
+    margin-bottom: 8px;
+`;
+
+export const ProductActionRowTitle = styled.div`
+    display: flex;
+    gap: 20px;
+    align-items: center;
+
+    h1 {
+        width: 100%;
+        font-size: 26px;
+        font-weight: 600;
+    }
+
+    svg {
+        cursor: pointer;
+    }
+`;
+
+export const ProductActionPriceCard = styled.div`
+    margin-top: 18px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ProductActionPriceRow = styled.div`
+    display: flex;
+    align-items: flex-start;
+    font-size: 44px;
+    font-weight: 300;
+
+    .fraction {
+        margin-left: 8.8px;
+    }
+
+    .cents {
+        font-size: 20px;
+        padding-top: 3px;
+        margin-left: 1px;
+    }
+`;
+
+export const ProductActionContainer = styled.div`
+    padding: 32px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+`;
+
+export const ProductActionMethodCard = styled.div`
+    margin-top: 18px;
+    display: flex;
+    cursor: pointer;
+
+    > div {
+        margin-left: 8px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .title {
+        color: #00a650;
+        font-size: 16px;
+    }
+
+    .details {
+        margin-top: 5px;
+        color: #666;
+        font-size: 14px;
+    }
+
+    .more {
+        margin-top: 5px;
+        font-size: 14px;
+        font-weight: 600;
+        text-decoration: none;
+        color: #3483fa;
+    }
+`;
+
+export const ProductActionActions = styled.div`
+    margin-top: 16px;
+    display: flex;
+    flex-direction: column;
+`;
+
+
+export const ProductActionButton = styled.button.attrs<React.ButtonHTMLAttributes<HTMLButtonElement> & { solid?: boolean }>((props) => ({
+  solid: undefined,
+}))<{ solid?: boolean }>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 15px;
+    border-radius: 4px;
+    padding: 12px 10px;
+    margin-top: 10px;
+    cursor: pointer;
+    transition: background 0.3s, color 0.3s, border 0.3s;
+
+    background: ${({ solid }) => (solid ? '#3483fa' : 'transparent')};
+    color: ${({ solid }) => (solid ? '#FFF' : '#3483fa')};
+    border: ${({ solid }) => (solid ? 'none' : '1px solid #3483fa')};
+`;
+
+export const ProductActionBenefits = styled.ul`
+    margin-top: 16px;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+
+    li {
+        display: flex;
+        align-items: center;
+
+        p {
+            margin-left: 10px;
+            color: #666;
+            font-size: 14px;
+        }
+    }
+`;
+
+export const SellerInfoContainer = styled.div`
+    padding: 48px 32px;
+    display: flex;
+    flex-direction: column;
+    border-top: 1px solid #ddd;
+`;
+
+export const SellerInfoTitle = styled.div`
+    font-size: 18px;
+    margin-bottom: 20px;
+    font-weight: 600;
+`;
+
+export const SellerInfoLocationCard = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 7px 0;
+
+    > div {
+        margin-left: 8px;
+
+        p {
+            font-size: 16px;
+        }
+
+        strong {
+            font-size: 14px;
+            font-weight: normal;
+            color: #666;
+        }
+    }
+`;
+
+export const SellerInfoReputationCard = styled.div`
+    margin-top: 33px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const SellerInfoReputationThermometer = styled.ol`
+    list-style: none;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 7px;
+    padding: 0 4px;
+
+    li {
+        width: 100%;
+        height: 8px;
+
+        &:nth-child(1) {
+            background: #FFE7E6;
+        }
+
+        &:nth-child(2) {
+            background: #FFF4E7;
+        }
+
+        &:nth-child(3) {
+            background: #FFFDE5;
+        }
+
+        &:nth-child(4) {
+            background: #F3FEE0;
+        }
+
+        &:nth-child(5) {
+            background: #00A650;
+            height: 12px;
+        }
+
+        &.active {
+            opacity: 1;
+        }
+    }
+`;
+
+export const SellerInfoReputationRow = styled.div`
+    margin-top: 10px;
+    display: flex;
+    align-items: flex-start;
+    width: 100%;
+
+    > div {
+        width: 33%;
+        padding: 0 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        position: relative;
+
+        strong {
+            font-size: 24px;
+            font-weight: normal;
+            height: 30px;
+        }
+
+        span {
+            font-size: 12px;
+        }
+
+        & + div::before {
+            content: "";
+            position: absolute;
+            left: 0;
+            top: 50%;
+            transform: translateY(-50%);
+            height: 36px;
+            border-left: 1px solid #ddd;
+        }
+    }
+`;
+
+export const SellerInfoMore = styled.a`
+    margin-top: 24px;
+    color: #3483fa;
+    font-size: 14px;
+    font-weight: 600;
+    text-decoration: none;
+    cursor: pointer;
+`;
+
+
+export const InfoContainer = styled.div`
+    border-top: 1px solid #ddd;
+    padding: 44px 32px;
+`;
+
+export const InfoTitle = styled.h2`
+    font-size: 24px;
+    margin-bottom: 32px;
+`;
+
+export const InfoDescription = styled.p`
+    font-size: 20px;
+    color: #666;
+    line-height: 27px;
+`;
+
+
+export const StyledSellerInfo = styled.div`
+    padding: 48px 32px;
+    display: flex;
+    flex-direction: column;
+    border-top: 1px solid #ddd;
+`;
+
+export const SellerInfoInner = styled.div``;
